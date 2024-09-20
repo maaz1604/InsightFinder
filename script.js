86fc2df3c9f82cf11e813b4e9f13e055dbf4dbe8
Maaz Amir <133258695+maaz1604@users.noreply.github.com>
2024-09-20 21:24:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function displayBlogs(articles) {
         const blogCard = document.createElement("div")
         blogCard.classList.add("blog-card")
         const img = document.createElement("img")
-        img.src = article.urlToImage || "default-image.jpg"; // Fallback if no image
+        img.src = article.urlToImage || "default-image.jpeg"; // Fallback if no image
         img.alt = article.title;
         const title = document.createElement("h2");
         const TruncatedTitle = article.title.length>30? article.title.slice(0,30) + "....":article.title;
